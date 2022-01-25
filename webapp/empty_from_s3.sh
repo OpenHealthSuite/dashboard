7a0b0812e93e24429d7e33bf981fc6fc3d2ca439
Lee Martin <lee.martin@pliminus.co.uk>
2022-01-25 21:40:40 +0000

    Delete: Old empty/push scripts

--- webapp/empty_from_s3.sh
+++
@@ -1,4 +0,0 @@
-export FRONTEND_BUCKET=$(cat ../.infrastructure/cdk.out/outputs.json | jq -r '.["PaceMeFrontendStack"].frontendBucketName')
-export BUCKET_REGION=$(cat ../.infrastructure/cdk.out/outputs.json | jq -r '.["PaceMeFrontendStack"].frontendBucketRegion')
-
-aws s3 rm s3://$FRONTEND_BUCKET --recursive --region $BUCKET_REGION
