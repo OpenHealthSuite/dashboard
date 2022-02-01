ea181bd4ac387c5824c731ffc0aaedcbeb752e73
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-01 22:15:21 +0000

    Sweeping set of changes for frontend

--- webapp/src/components/dashboard/ActivityDashboard.tsx
+++ webapp/src/components/dashboard/ActivityDashboard.tsx
@@ -8,11 +8,11 @@ import { getTodaysSteps, getTodaysCalories, ISleep, getTodaySleep, IDatedSteps, 
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { CardHeader } from '@mui/material';
 
 interface IDashboardState {
     loading: boolean,
@@ -46,17 +46,97 @@ function dateDaysMatch(dateOne: Date, dateTwo: Date): boolean {
 }
 
 interface IDashboardTileProps {
+    headerText?: string,
     children: ReactNode
 }
 
 function DashboardTile(props: IDashboardTileProps) {
+    const cardHeader = props.headerText ? <CardHeader title={props.headerText}/> : null
     return (<Card>
+        {cardHeader}
         <CardContent>
-            <Typography>
-                {props.children}
-            </Typography>
+            {props.children}
         </CardContent>
     </Card>)
+}
+
+const dayLabels = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+]
+
+const colors = {
+    caloriesIn: "#3A3",
+    caloriesOut: "#A33",
+    steps: "#44C"
+}
+
+function CaloriesGraphTile( props: { caloriesArray: IDatedCaloriesInOut[] }) {
+    const caloriesInTotal= props.caloriesArray.map(x => x.caloriesIn - 0).reduce((partial, a) => a + partial, 0)
+    const caloriesOutTotal= props.caloriesArray.map(x => x.caloriesOut - 0).reduce((partial, a) => a + partial, 0)
+    const caloriesDelta= caloriesInTotal - caloriesOutTotal
+    const caloriesString = `In: ${caloriesInTotal.toLocaleString()}  |  Out: ${caloriesOutTotal.toLocaleString()}  |  Delta: ${caloriesDelta.toLocaleString()}`
+    return (<DashboardTile headerText='Last Week Calories'>
+            <>
+                <ResponsiveContainer width="100%" height={250}>
+                        <BarChart
+                            
+                            data={props.caloriesArray.map(x => { 
+                                return { 
+                                    dateLabel: dayLabels[new Date(x.date).getDay()],
+                                    caloriesIn: x.caloriesIn,
+                                    caloriesOut: x.caloriesOut
+                                }})}
+                            margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                            >
+                            <XAxis dataKey="dateLabel"/>
+                            <YAxis orientation="right"/>
+                            <Bar type="monotone" dataKey="caloriesIn" fill={colors.caloriesIn} yAxisId={0} />
+                            <Bar type="monotone" dataKey="caloriesOut" fill={colors.caloriesOut} yAxisId={0} />
+                        </BarChart>
+                </ResponsiveContainer>
+                <p style={{textAlign:"center"}}>{caloriesString}</p>
+            </>
+    </DashboardTile>)
+}
+function StepsGraphTile(props: { stepsArray: IDatedSteps[] }) {
+    const totalSteps= props.stepsArray.map(x => x.steps - 0).reduce((partial, a) => a + partial, 0)
+    return (<DashboardTile headerText='Last Week Steps'>
+        <>
+            <ResponsiveContainer width="100%" height={250}>
+                <BarChart
+                    
+                    data={props.stepsArray.map(x => { 
+                        return { 
+                            dateLabel: dayLabels[new Date(x.date).getDay()],
+                            steps: x.steps
+                        }})}
+                    margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                    >
+                    <XAxis dataKey="dateLabel"/>
+                    <YAxis orientation="right"/>
+                    <Bar type="monotone" dataKey="steps" fill={colors.steps} yAxisId={0} />
+                </BarChart>
+            </ResponsiveContainer>
+            <p style={{textAlign:"center"}}>{totalSteps.toLocaleString()} Total Steps | {Math.floor(totalSteps / props.stepsArray.length).toLocaleString()} Avg.</p>
+        </>
+    </DashboardTile>)
+}
+
+function formatMinutesToText(minutes: number, startString: string = ""): string {
+    if (minutes === 0) {
+        return startString
+    }
+    if (minutes < 60) {
+        return `${startString} ${minutes} minutes`
+    }
+    const hours = Math.floor(minutes / 60)
+    return formatMinutesToText(minutes - (hours * 60), `${hours} hours`)
 }
 
 export default class ActivityDashboard extends React.Component<{}, IDashboardState> {
@@ -127,46 +207,30 @@ export default class ActivityDashboard extends React.Component<{}, IDashboardSta
     }
 
     render(): React.ReactNode {
-        const dayLabels = [
-            "Sun",
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thu",
-            "Fri",
-            "Sat"
-        ]
+        const todayCalorieDelta = (this.state.caloriesIn - this.state.caloriesOut)
         let content = <Grid container spacing={2}>
                 <Grid item xs={4}>
-                    <DashboardTile>
-                        <h1>Calories</h1>
-                        <div>
-                            <h2>{this.state.caloriesIn}</h2>
-                            <h2>{this.state.caloriesOut}</h2>
+                    <DashboardTile headerText='Today'>
+                        <div style={{textAlign:"center"}}>
+                            <h1>
+                                <span style={{color: colors.caloriesIn}}>{this.state.caloriesIn.toLocaleString()} In</span> - <span style={{color: colors.caloriesOut}}>{this.state.caloriesOut.toLocaleString()} Out</span>
+                            </h1>
+                            <h2 style={{color: todayCalorieDelta < 0 ? colors.caloriesOut : colors.caloriesIn}}>{todayCalorieDelta.toLocaleString()} Calories Delta</h2>
+                            <h2 style={{color: colors.steps}}>{this.state.stepCount.toLocaleString()} Steps</h2>
                         </div>
                     </DashboardTile>
                 </Grid>
                 <Grid item xs={4}>
-                    <DashboardTile>
-                        <h1>Steps</h1>
+                    <DashboardTile headerText='Sleep'>
                         <div>
-                            <h2>{this.state.stepCount}</h2>
+                            <h1>{formatMinutesToText(this.state.sleep.asleep)}</h1>
+                            <h3>REM: {formatMinutesToText(this.state.sleep.rem)}</h3>
+                            <h4>Awake: {formatMinutesToText(this.state.sleep.awake)}</h4>
                         </div>
                     </DashboardTile>
                 </Grid>
                 <Grid item xs={4}>
-                    <DashboardTile>
-                        <h1>Sleep</h1>
-                        <div>
-                            <h2>Asleep: {this.state.sleep.asleep}</h2>
-                            <h2>REM: {this.state.sleep.rem}</h2>
-                            <h4>Awake: {this.state.sleep.awake}</h4>
-                        </div>
-                    </DashboardTile>
-                </Grid>
-                <Grid item xs={4}>
-                    <DashboardTile>
-                    <h1>Activities</h1>
+                    <DashboardTile headerText='Activities'>
                     {this.state.pendingActivities.map((x, i) => 
                         <div key={`pendact-${i}`}>
                             <h2>{x.activityTime.toISOString().split('T')[0]}</h2>
@@ -180,46 +244,10 @@ export default class ActivityDashboard extends React.Component<{}, IDashboardSta
                     </DashboardTile>
                 </Grid>
                 <Grid item xs={4}>
-                    <DashboardTile>
-                        <h1>Last Week Steps</h1>
-                        <ResponsiveContainer width="100%" height={250}>
-                            <BarChart
-                                
-                                data={this.state.lastWeekSteps.map(x => { 
-                                    return { 
-                                        dateLabel: dayLabels[new Date(x.date).getDay()],
-                                        steps: x.steps
-                                    }})}
-                                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-                                >
-                                <XAxis dataKey="dateLabel"/>
-                                <YAxis orientation="right"/>
-                                <Bar type="monotone" dataKey="steps" fill="#44C" yAxisId={0} />
-                            </BarChart>
-                        </ResponsiveContainer>
-                    </DashboardTile>
+                    <StepsGraphTile stepsArray={this.state.lastWeekSteps}/>
                 </Grid>
                 <Grid item xs={4}>
-                    <DashboardTile>
-                        <h1>Last Week Calories</h1>
-                        <ResponsiveContainer width="100%" height={250}>
-                            <BarChart
-                                
-                                data={this.state.lastWeekCalories.map(x => { 
-                                    return { 
-                                        dateLabel: dayLabels[new Date(x.date).getDay()],
-                                        caloriesIn: x.caloriesIn,
-                                        caloriesOut: x.caloriesOut
-                                    }})}
-                                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-                                >
-                                <XAxis dataKey="dateLabel"/>
-                                <YAxis orientation="right"/>
-                                <Bar type="monotone" dataKey="caloriesIn" fill="#3A3" yAxisId={0} />
-                                <Bar type="monotone" dataKey="caloriesOut" fill="#A33" yAxisId={0} />
-                            </BarChart>
-                        </ResponsiveContainer>
-                    </DashboardTile>
+                    <CaloriesGraphTile caloriesArray={this.state.lastWeekCalories} />
                 </Grid>
             </Grid>
         
