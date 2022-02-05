c54064b07acefe0c9b8600e6d8982d7a85ffb844
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-05 16:35:07 +0000

    Added: Standalone Calorie Graph Tile

---
+++ webapp/src/components/dashboard/tiles/CaloriesGraphTile.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+
+import { getDateRangeCalories, IDatedCaloriesInOut } from '../../../services/StatsService'
+import { DashboardTile } from './BaseTile'
+
+const dayLabels = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"
+]
+
+const colors = {
+  caloriesIn: "#3A3",
+  caloriesOut: "#A33",
+  steps: "#44C"
+}
+
+const yesterDate = new Date()
+yesterDate.setDate(yesterDate.getDate() - 1)
+const lastWeekDate = new Date()
+lastWeekDate.setDate(lastWeekDate.getDate() - 7)
+
+interface CaloriesGraphTileProps {
+  fnGetDateRangeCalories?: (startDate: Date, endDate: Date) => Promise<IDatedCaloriesInOut[]>
+}
+
+export function CaloriesGraphTile({ fnGetDateRangeCalories = getDateRangeCalories }: CaloriesGraphTileProps) {
+  const [caloriesArray, setCaloriesArray] = useState<IDatedCaloriesInOut[]>([])
+  
+  useEffect(() => {
+    const getCalories = async () => {
+      setCaloriesArray(await fnGetDateRangeCalories(lastWeekDate, yesterDate))
+    }
+    if(caloriesArray.length === 0) {
+      getCalories()
+    }
+  }, [caloriesArray.length, fnGetDateRangeCalories])
+
+  const caloriesInTotal= caloriesArray.map(x => x.caloriesIn - 0).reduce((partial, a) => a + partial, 0)
+  const caloriesOutTotal= caloriesArray.map(x => x.caloriesOut - 0).reduce((partial, a) => a + partial, 0)
+  const caloriesDelta= caloriesInTotal - caloriesOutTotal
+  const caloriesString = `In: ${caloriesInTotal.toLocaleString()}  |  Out: ${caloriesOutTotal.toLocaleString()}  |  Delta: ${caloriesDelta.toLocaleString()}`
+  return (<DashboardTile headerText='Last Week Calories' loading={caloriesArray.length === 0}>
+          <>
+              <ResponsiveContainer width="100%" height={250}>
+                      <BarChart
+                          
+                          data={caloriesArray.map(x => { 
+                              return { 
+                                  dateLabel: dayLabels[new Date(x.date).getDay()],
+                                  caloriesIn: x.caloriesIn,
+                                  caloriesOut: x.caloriesOut
+                              }})}
+                          margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                          >
+                          <XAxis dataKey="dateLabel"/>
+                          <YAxis orientation="right"/>
+                          <Bar type="monotone" dataKey="caloriesIn" fill={colors.caloriesIn} yAxisId={0} />
+                          <Bar type="monotone" dataKey="caloriesOut" fill={colors.caloriesOut} yAxisId={0} />
+                      </BarChart>
+              </ResponsiveContainer>
+              <p style={{textAlign:"center"}}>{caloriesString}</p>
+          </>
+  </DashboardTile>)
+}
