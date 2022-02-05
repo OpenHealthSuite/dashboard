913a93c0235b9c67fc43038afc86f7b99c053fc4
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-05 20:02:15 +0000

    Added: Refresh bar for tiles

--- webapp/src/components/dashboard/tiles/BaseTile.tsx
+++ webapp/src/components/dashboard/tiles/BaseTile.tsx
@@ -5,11 +5,16 @@ import { LoadingIndicator } from '../../shared/LoadingIndicator';
 interface IDashboardTileProps {
   headerText?: string,
   children: ReactNode,
-  loading?: boolean
+  loading?: boolean,
+  refreshDetails?: {
+    refreshInterval: number,
+    remaining: number
+  }
 }
 
-export function DashboardTile({ headerText, children, loading = false }: IDashboardTileProps) {
+export function DashboardTile({ headerText, children, loading = false, refreshDetails }: IDashboardTileProps) {
   const cardHeader = headerText ? <CardHeader title={headerText} /> : null
+  const refreshBar = refreshDetails ? <div style={{ height: "0.5em", width: `${(refreshDetails.remaining / refreshDetails.refreshInterval) * 100}%`, backgroundColor: "rgba(255,255,255,0.2)" }}></div> : null
   return (<Card>
     {cardHeader}
     <CardContent>
@@ -17,6 +22,6 @@ export function DashboardTile({ headerText, children, loading = false }: IDashbo
         {children}
       </LoadingIndicator>
     </CardContent>
-
+    {refreshBar}
   </Card>)
 }
