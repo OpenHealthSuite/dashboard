4185a053c14cb461bbfa97d2ce839795169ae744
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-05 16:34:36 +0000

    Added: New Base Dashboard Tile

---
+++ webapp/src/components/dashboard/tiles/BaseTile.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import { CardHeader, Card, CardContent } from '@mui/material';
+import { LoadingIndicator } from '../../shared/LoadingIndicator';
+
+interface IDashboardTileProps {
+  headerText?: string,
+  children: ReactNode,
+  loading?: boolean
+}
+
+export function DashboardTile({ headerText, children, loading = false }: IDashboardTileProps) {
+  const cardHeader = headerText ? <CardHeader title={headerText}/> : null
+  return (<Card>
+        {cardHeader}
+        <CardContent>
+          <LoadingIndicator loading={loading}>
+              {children}
+          </LoadingIndicator>
+        </CardContent>
+  </Card>)
+}
