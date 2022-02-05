a766282484ba89f768226518bcb84b262db1b8ca
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-05 20:23:14 +0000

    Added: Autorefresh to calories graph

--- webapp/src/components/dashboard/tiles/CaloriesGraphTile.tsx
+++ webapp/src/components/dashboard/tiles/CaloriesGraphTile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 import { getDateRangeCalories, IDatedCaloriesInOut } from '../../../services/StatsService'
@@ -31,21 +31,35 @@ interface CaloriesGraphTileProps {
 
 export function CaloriesGraphTile({ fnGetDateRangeCalories = getDateRangeCalories }: CaloriesGraphTileProps) {
   const [caloriesArray, setCaloriesArray] = useState<IDatedCaloriesInOut[]>([])
+  const refreshIntervalMilliseconds = 300000;
+  const [refreshRemaining, setRefreshRemaining] = useState<number>(refreshIntervalMilliseconds)
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false)
+
+  useEffect(() => {
+    const getInitialCalories = async () => { setCaloriesArray(await fnGetDateRangeCalories(lastWeekDate, yesterDate)) }
+    getInitialCalories()
+  }, [setCaloriesArray, fnGetDateRangeCalories])
 
   useEffect(() => {
     const getCalories = async () => {
+      setIsRefreshing(true)
       setCaloriesArray(await fnGetDateRangeCalories(lastWeekDate, yesterDate))
+      setIsRefreshing(false)
     }
-    if (caloriesArray.length === 0) {
-      getCalories()
-    }
-  }, [caloriesArray.length, fnGetDateRangeCalories])
+    const timer = setTimeout(() => {
+      setRefreshRemaining(refreshRemaining - 500)
+      if (refreshRemaining < 0 && !isRefreshing) {
+        getCalories()
+      }
+    }, 500)
+    return () => clearTimeout(timer);
+  }, [isRefreshing, refreshRemaining, setRefreshRemaining, setCaloriesArray, fnGetDateRangeCalories])
 
   const caloriesInTotal = caloriesArray.map(x => x.caloriesIn - 0).reduce((partial, a) => a + partial, 0)
   const caloriesOutTotal = caloriesArray.map(x => x.caloriesOut - 0).reduce((partial, a) => a + partial, 0)
   const caloriesDelta = caloriesInTotal - caloriesOutTotal
   const caloriesString = `In: ${caloriesInTotal.toLocaleString()}  |  Out: ${caloriesOutTotal.toLocaleString()}  |  Delta: ${caloriesDelta.toLocaleString()}`
-  return (<DashboardTile headerText='Last Week Calories' loading={caloriesArray.length === 0}>
+  return (<DashboardTile headerText='Last Week Calories' loading={caloriesArray.length === 0} refreshDetails={{refreshInterval: refreshIntervalMilliseconds, remaining: refreshRemaining}}>
     <>
       <ResponsiveContainer width="100%" height={250}>
         <BarChart
