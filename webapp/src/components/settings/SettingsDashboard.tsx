3bf04dc09d41f0ad65af1308374dc7028313371c
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-06 22:45:38 +0000

    Making settings a bit more pleasant

--- webapp/src/components/settings/SettingsDashboard.tsx
+++ webapp/src/components/settings/SettingsDashboard.tsx
@@ -1,32 +1,47 @@
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { getProviderStatuses, startChallenge, IProviderStatus } from '../../services/ProvidersService'
+import { LoadingIndicator } from '../shared/LoadingIndicator';
 
 export default function SettingsDashboard() {
-  const [statuses, setStatuses] = useState<IProviderStatus[]>([]);
-  const [loading, setLoading] = useState(true)
-
-  const getStatuses = async () => {
-      setLoading(false)
-      // TODO: Graceful error handling with toast
-      setStatuses((await getProviderStatuses()) || [])
-  }
-
-  useEffect(() => {
-      if (loading) {
-          getStatuses()
-      }
-  }, [loading])
-
-  return <ul>
-      {statuses.map(s => <li key={s.key}>{s.name} - {s.authenticated ? 'Authed':'Unauthed'}: <button onClick={() => GetChallenge(s.key)}>Authenticate</button></li>)}
-      </ul>
+    return (<Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4}>
+            <ProviderSettings />
+        </Grid>
+    </Grid>)
 }
 
+function ProviderSettings() {
+    const [statuses, setStatuses] = useState<IProviderStatus[]>([]);
+    const [loading, setLoading] = useState(true)
+
+    const getStatuses = async () => {
+        setLoading(false)
+        // TODO: Graceful error handling with toast
+        setStatuses((await getProviderStatuses()) || [])
+    }
+
+    useEffect(() => {
+        if (loading) {
+            getStatuses()
+        }
+    }, [loading])
+
+    return (<Card>
+        <CardHeader title={"Data Providers"} />
+        <CardContent>
+            <LoadingIndicator loading={loading}>
+                <ul>
+                    {statuses.map(s => <li key={s.key}>{s.name} - {s.authenticated ? 'Authed' : 'Unauthed'}: <button onClick={() => GetChallenge(s.key)}>Authenticate</button></li>)}
+                </ul>
+            </LoadingIndicator></CardContent>
+    </Card>)
+}
 
 async function GetChallenge(key: string) {
-  // Send post to api.address/users/:userId/providers/:key/start to get URL w/ challenge
-  // Send user to retreived URL
-  const { authUrl } = await startChallenge(key)
+    // Send post to api.address/users/:userId/providers/:key/start to get URL w/ challenge
+    // Send user to retreived URL
+    const { authUrl } = await startChallenge(key)
 
-  window.location.href = authUrl
+    window.location.href = authUrl
 }
