c8d01a44c6ad4152eca813bf0726d66f0de91f3b
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-06 20:50:14 +0000

    Update: Pulling dash settings to a single object

--- webapp/src/components/dashboard/ActivityDashboard.tsx
+++ webapp/src/components/dashboard/ActivityDashboard.tsx
@@ -6,39 +6,40 @@ import { SleepDailyTile } from './tiles/SleepDailyTile'
 
 import  { Grid, GridSize } from '@mui/material';
 
-interface DashboardSettings { 
+interface ITileSettings { 
+    component: (props: any) => JSX.Element
+}
+
+interface IDashboardSettings { 
     spacing: number, 
     tileSizes: { 
         xs: GridSize, 
         sm: GridSize, 
         md: GridSize
-    }
+    },
+    tiles: ITileSettings[]
 }
 
-interface TileSettings { 
-    component: (props: any) => JSX.Element
-}
 
 export default function ActivityDashboard() {
-    const dashboardSettings: DashboardSettings = {
+    const dashboardSettings: IDashboardSettings = {
         spacing: 2,
         tileSizes: {
             xs: 12,
             sm: 6,
             md: 4
-        }
+        },
+        tiles: [
+            { component: ActivitiesTile },
+            { component: CaloriesStepsDailyTile },
+            { component: SleepDailyTile },
+            { component: StepsGraphTile },
+            { component: CaloriesGraphTile }
+        ]
     }
 
-    const tiles: TileSettings[] = [
-        { component: ActivitiesTile },
-        { component: CaloriesStepsDailyTile },
-        { component: SleepDailyTile },
-        { component: StepsGraphTile },
-        { component: CaloriesGraphTile }
-    ]
-
     return (<Grid container spacing={dashboardSettings.spacing}>
-        {tiles.map((tile, i) => { 
+        {dashboardSettings.tiles.map((tile, i) => { 
             return <Grid key={`gridkey-${i}`} item xs={dashboardSettings.tileSizes.xs} sm={dashboardSettings.tileSizes.sm} md={dashboardSettings.tileSizes.md}>
                 <tile.component />
             </Grid> })}
