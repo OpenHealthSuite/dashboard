6f6d5aa5728b052a594434a1c7815006520d8b5e
Lee Martin <lee.martin@pliminus.co.uk>
2022-02-12 15:21:34 +0000

    Fix: ECR access doesn't need the namespace to already exist

--- api/scripts/kubernetes-ecr-access.sh
+++ api/scripts/kubernetes-ecr-access.sh
@@ -2,6 +2,7 @@
 
 export REPOSITORY=$(credstash get paceme/ECRApiRepository)
 export REPOSITORY_ROOT="$( cut -d '/' -f 1 <<< "$REPOSITORY" )"
+kubectl create namespace paceme-api || true
 kubectl delete secret awsregcred --namespace=paceme-api || true
 kubectl create secret docker-registry awsregcred \
   --docker-server=$REPOSITORY_ROOT\
