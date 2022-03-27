b37cb4de4623a5df4a89cabf2010cf4666398b28
Lee Martin <lee.martin@pliminus.co.uk>
2022-03-27 21:02:41 +0100

    Ripping out Frontend CDK

--- .infrastructure/bin/.infrastructure.ts
+++ .infrastructure/bin/.infrastructure.ts
@@ -2,7 +2,6 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PaceMeScaffoldStack } from '../lib/pace-me-scaffold-stack';
-import { PaceMeFrontendStack } from '../lib/pace-me-frontend-stack';
 import { joinTruthyStrings } from '../utilities/stringjoiner'
 
 const STACK_ENVIRONMENT = process.env.CDK_ENVIRONMENT === "Production" ? undefined : "Development"
@@ -11,10 +10,5 @@ const STACK_PREFIX = process.env.CDK_ENVIRONMENT === "Production" ? undefined : 
 const app = new cdk.App();
 new PaceMeScaffoldStack(app, joinTruthyStrings(['PaceMeScaffoldStack', STACK_ENVIRONMENT]), {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  stackPrefix: STACK_PREFIX
-});
-
-new PaceMeFrontendStack(app, joinTruthyStrings(['PaceMeFrontendStack', STACK_ENVIRONMENT]), {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
   stackPrefix: STACK_PREFIX
 });
