cf60efb6041060901736d8ab33879730f9c0bad2
Lee Martin <lee.martin@pliminus.co.uk>
2022-03-27 21:04:01 +0100

    Removing Private ECR Repo

--- .infrastructure/lib/pace-me-scaffold-stack.ts
+++ .infrastructure/lib/pace-me-scaffold-stack.ts
@@ -1,15 +1,8 @@
-import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dyn from 'aws-cdk-lib/aws-dynamodb'
 import * as cdk from 'aws-cdk-lib';
 import * as cognito from 'aws-cdk-lib/aws-cognito'
-import * as ecr from 'aws-cdk-lib/aws-ecr'
-// import * as ecs from 'aws-cdk-lib/aws-ecs'
-import * as iam from 'aws-cdk-lib/aws-iam'
-import { TagStatus } from 'aws-cdk-lib/aws-ecr';
-import { joinTruthyStrings } from '../utilities/stringjoiner';
-// import * as ecrdeploy from 'cdk-ecr-deployment';
-// import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
 
 interface PaceMeScaffoldStackProps extends StackProps {
   stackPrefix: string | undefined
@@ -99,49 +92,5 @@ export class PaceMeScaffoldStack extends Stack {
         exportName: `cognitoLocalWebappClient`
       }
     )
-
-    const ecrRepo = new ecr.Repository(this, 
-    id + 'ApiRepository', 
-    { 
-      repositoryName: (joinTruthyStrings([props.stackPrefix, "paceme-api"])).toLowerCase(),
-      removalPolicy: RemovalPolicy.DESTROY
-    })
-    ecrRepo.addLifecycleRule({ tagStatus: TagStatus.UNTAGGED, maxImageAge: Duration.days(1) })
-
-    new cdk.CfnOutput(
-      this, 
-      `ECR-ApiRepository`, 
-      {
-        value: ecrRepo.repositoryUri,
-        description:`ECR Repository for PaceMe API`,
-        exportName: `ecrRepositoryUri`
-      }
-    )
-    
-    // I want this, but it seems like building to a platform isn't working?
-    // const image = new DockerImageAsset(this, 'CDKDockerImage', { 
-    //   directory: '../api',
-    //   buildArgs: {
-    //   // TODO: This couples us to the raspberry pi 32 bit...
-    //     platform: 'linux/arm64/v8'
-    //   }})
-
-    // new ecrdeploy.ECRDeployment(this, 'DeployDockerImage', {
-    //   src: new ecrdeploy.DockerImageName(image.imageUri),
-    //   dest: new ecrdeploy.DockerImageName(ecrRepo.repositoryUri + ':latest')
-    // } )
-
-    const awsApiUser = new iam.User(this, 'ApiUser', { })
-
-    createdTables.map(x => x.table.grantFullAccess(awsApiUser))
-
-    ecrRepo.grantPull(awsApiUser)
-
-    const accessKey = new iam.CfnAccessKey(this, 'PaceMeApiAWSAccessKey', {
-      userName: awsApiUser.userName
-    });
-
-    new cdk.CfnOutput(this, 'ApiAwsAccessKey', { value: accessKey.ref });
-    new cdk.CfnOutput(this, 'ApiAwsSecretKey', { value: accessKey.attrSecretAccessKey });
   }
 }
