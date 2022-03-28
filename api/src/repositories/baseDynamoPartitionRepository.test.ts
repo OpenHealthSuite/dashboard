d7da93a310793819a5dc62156cfda2e2ad60c21d
Lee Martin <lee.martin@pliminus.co.uk>
2022-03-28 21:48:13 +0100

    Deleteing the unused dynamo code

--- api/src/repositories/baseDynamoPartitionRepository.test.ts
+++ api/src/repositories/baseDynamoPartitionRepository.test.ts
@@ -34,100 +34,6 @@ test('baseDynamoRepository.create :: Overall Happy Test :: Creates new item', as
   fakeDynamoClient.put.calledOnceWithExactly(expectedParams)
 })
 
-test('baseDynamoRepository.getAll :: Overall Happy Test :: Gets and returns items ', async () => {
-  const testTableName = 'TESTTABLE'
-
-  const expectedUserId = '456EXPECTEDUSERID'
-
-  const mockItems = [{ id: '123TESTEXPECTEDID', userId: expectedUserId }]
-
-  const expectedPartitionKey = 'userId'
-
-  const expectedExpressionAttributes = {
-    '#nm': 'name',
-    '#active': 'active'
-  }
-
-  const expectedParams = {
-    TableName: testTableName,
-    ProjectionExpression: 'userId, #nm, #active',
-    ExpressionAttributeNames: expectedExpressionAttributes
-  }
-
-  const fakeDynamoClient = { scan: sinon.stub().returns({ promise: sinon.stub().resolves({ Items: mockItems }) }) }
-
-  // Act
-  const result = await repository.getAll(testTableName, expectedPartitionKey, expectedExpressionAttributes, fakeDynamoClient as unknown as dynamodb.DocumentClient)
-
-  // Assert
-
-  expect(result).toEqual(mockItems)
-  fakeDynamoClient.scan.calledOnceWithExactly(expectedParams)
-})
-
-test('baseDynamoRepository.getByPrimaryKey :: Overall Happy Test :: Gets item and returns it ', async () => {
-  const testTableName = 'TESTTABLE'
-
-  const expectedUserId = '456EXPECTEDUSERID'
-
-  const expectedPartitionKey = 'userId'
-
-  const mockItem = { Item: { id: '123TESTEXPECTEDID', userId: '456EXPECTEDUSERID' } }
-
-  const expectedParams = {
-    TableName: testTableName,
-    Key: {
-      userId: expectedUserId
-    }
-  }
-
-  const fakeDynamoClient = { get: sinon.stub().returns({ promise: sinon.stub().resolves({ Item: mockItem }) }) }
-
-  // Act
-  const result = await repository.getByPrimaryKey(testTableName, expectedPartitionKey, expectedUserId, fakeDynamoClient as unknown as dynamodb.DocumentClient)
-
-  // Assert
-
-  expect(result).toEqual(mockItem)
-  fakeDynamoClient.get.calledOnceWithExactly(expectedParams)
-})
-
-test('baseDynamoRepository.getAllByParentKey :: Overall Happy Test :: Gets and returns items ', async () => {
-  const testTableName = 'TESTTABLE'
-
-  const expectedUserId = '456EXPECTEDUSERID'
-
-  const mockItems = [{ id: '123TESTEXPECTEDID', userId: expectedUserId }]
-
-  const expectedPartitionKey = 'userId'
-  const expectedSortKey = 'id'
-
-  const expectedExpressionAttributes = {
-    '#nm': 'name',
-    '#active': 'active'
-  }
-
-  const expectedParams = {
-    TableName: testTableName,
-    ProjectionExpression: 'userId, id, #nm, #active',
-    FilterExpression: 'userId = :requestedPartitionKey',
-    ExpressionAttributeNames: expectedExpressionAttributes,
-    ExpressionAttributeValues: {
-      ':requestedPartitionKey': expectedUserId
-    }
-  }
-
-  const fakeDynamoClient = { scan: sinon.stub().returns({ promise: sinon.stub().resolves({ Items: mockItems }) }) }
-
-  // Act
-  const result = await repository.getAllByParentKey(testTableName, expectedPartitionKey, expectedSortKey, expectedUserId, expectedExpressionAttributes, fakeDynamoClient as unknown as dynamodb.DocumentClient)
-
-  // Assert
-
-  expect(result).toEqual(mockItems)
-  fakeDynamoClient.scan.calledOnceWithExactly(expectedParams)
-})
-
 test('baseDynamoRepository.getByPrimaryAndParentKey :: Overall Happy Test :: Gets item and returns it ', async () => {
   const testTableName = 'TESTTABLE'
 
@@ -179,29 +85,6 @@ test('BaseDynamoRepository.update :: Overall Happy Test :: updates item ', async
 
   // Assert
   fakeDynamoClient.put.calledOnceWithExactly(expectedUpdateParams)
-})
-
-test('BaseDynamoRepository.delete :: Overall Happy Test :: deletes item ', async () => {
-  const testTableName = 'TESTTABLE'
-
-  const expectedUserId = '456EXPECTEDUSERID'
-
-  const expectedPartitionKey = 'userId'
-
-  const expectedParams = {
-    TableName: testTableName,
-    Key: {
-      userId: expectedUserId
-    }
-  }
-
-  const fakeDynamoClient = { delete: sinon.stub().returns({ promise: sinon.stub().resolves() }) }
-
-  // Act
-  await repository.deleteByPrimaryKey(testTableName, expectedPartitionKey, expectedUserId, fakeDynamoClient as unknown as dynamodb.DocumentClient)
-
-  // Assert
-  fakeDynamoClient.delete.calledOnceWithExactly(expectedParams)
 })
 
 test('BaseDynamoRepository.delete :: Overall Happy Test :: deletes item ', async () => {
--- api/src/repositories/baseDynamoPartitionRepository.ts
+++ api/src/repositories/baseDynamoPartitionRepository.ts
@@ -8,64 +8,10 @@ interface LooseKeyObject {
     [key: string]: string
 }
 
-export async function getAll<T> (tableName: string, partitionKey: string, expressionAttributeNames: LooseKeyObject, docClient: dynamodb.DocumentClient = _docClient): Promise<T[]> {
-  const params = {
-    TableName: tableName,
-    ProjectionExpression: `${partitionKey}, ${Object.keys(expressionAttributeNames).join(', ')}`,
-    ExpressionAttributeNames: expressionAttributeNames
-  }
-
-  const data = await docClient.scan(params).promise()
-  return data.Items as T[]
-}
-
-export async function getAllByParentKey<T> (tableName: string, partitionKey: string, sortKey: string, partitionKeyValue: string, expressionAttributeNames: LooseKeyObject, docClient: dynamodb.DocumentClient = _docClient): Promise<T[]> {
-  const params = {
-    TableName: tableName,
-    ProjectionExpression: `${partitionKey}, ${sortKey}, ${Object.keys(expressionAttributeNames).join(', ')}`,
-    FilterExpression: `${partitionKey} = :requestedPartitionKey`,
-    ExpressionAttributeNames: expressionAttributeNames,
-    ExpressionAttributeValues: {
-      ':requestedPartitionKey': partitionKeyValue
-    }
-  }
-
-  const data = await docClient.scan(params).promise()
-  return data.Items as T[]
-}
-
 export async function getByPrimaryAndParentKey<T> (tableName: string, partitionKey: string, partitionKeyValue: string, sortKey: string, sortKeyValue: string, docClient: dynamodb.DocumentClient = _docClient): Promise<T> {
   const key: LooseKeyObject = {}
   key[partitionKey] = partitionKeyValue
   key[sortKey as string] = sortKeyValue
-
-  const params = {
-    TableName: tableName,
-    Key: key
-  }
-
-  const data = await docClient.get(params).promise()
-  return data.Item as T
-}
-
-export async function getFilteredItems<T> (tableName: string, partitionKey: string, expressionAttributeNames: LooseKeyObject, property: string, value: string | number | undefined, docClient: dynamodb.DocumentClient = _docClient) {
-  const params = {
-    TableName: tableName,
-    ProjectionExpression: `${partitionKey}, ${Object.keys(expressionAttributeNames).join(', ')}`,
-    FilterExpression: `${property} = :propertyValue`,
-    ExpressionAttributeNames: expressionAttributeNames,
-    ExpressionAttributeValues: {
-      ':propertyValue': value
-    }
-  }
-
-  const data = await docClient.scan(params).promise()
-  return data.Items as T[]
-}
-
-export async function getByPrimaryKey<T> (tableName: string, partitionKey: string, paritionKeyValue: string, docClient: dynamodb.DocumentClient = _docClient): Promise<T> {
-  const key: LooseKeyObject = {}
-  key[partitionKey] = paritionKeyValue
 
   const params = {
     TableName: tableName,
@@ -93,17 +39,6 @@ export async function update<T> (tableName: string, itemUpdate: T, docClient: dy
   }
 
   await docClient.put(updateParams).promise()
-}
-
-export async function deleteByPrimaryKey (tableName: string, partitionKey: string, paritionKeyValue: string, docClient: dynamodb.DocumentClient = _docClient): Promise<void> {
-  const key: LooseKeyObject = {}
-  key[partitionKey] = paritionKeyValue
-  const deleteParams = {
-    TableName: tableName,
-    Key: key
-  }
-
-  await docClient.delete(deleteParams).promise()
 }
 
 export async function deleteByPrimaryAndParentKey (tableName: string, partitionKey: string, paritionKeyValue: string, sortKey: string, sortKeyValue: string, docClient: dynamodb.DocumentClient = _docClient): Promise<void> {
