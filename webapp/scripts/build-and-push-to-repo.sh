0649d2197bffc9b4e9b6f89af64f6336c6e7e373
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-01 00:30:21 +0100

    Build and push for webapp and helm updates

--- webapp/scripts/build-and-push-to-repo.sh
+++ webapp/scripts/build-and-push-to-repo.sh
@@ -5,4 +5,4 @@ export VERSION="$(cat package.json | jq -r '.version')"
 docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
   --build-arg REACT_APP_AWS_USER_POOL_ID="$(credstash get paceme/CognitoPoolId)" \
   --build-arg REACT_APP_AWS_USER_POOL_CLIENTID="$(credstash get paceme/CognitoLocalWebappClientId)" \
-  -t "$REPOSITORY:latest" --push . #-t "$REPOSITORY:$VERSION"
+  -t "$REPOSITORY:latest" -t "$REPOSITORY:$VERSION" --push . 
