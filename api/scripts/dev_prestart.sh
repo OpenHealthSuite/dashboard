3c1ea65c5a7500e4b66d30cf59b8da471cd50fe7
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-02 00:49:24 +0100

    Tweaking devstart to be agressive

--- api/scripts/dev_prestart.sh
+++ api/scripts/dev_prestart.sh
@@ -1,2 +1,2 @@
-docker rm paceme-redis || docker run --name paceme-redis -d -p 6379:6379 redis:6.2.6 || docker start paceme-redis
-docker rm paceme-mongo || docker run --name paceme-mongo -d -p 27017:27017 mongo:4.4.13 || docker start paceme-mongo
+docker start paceme-redis || docker run --name paceme-redis -d -p 6379:6379 redis:6.2.6
+docker start paceme-mongo || docker run --name paceme-mongo -d -p 27017:27017 mongo:4.4.13
