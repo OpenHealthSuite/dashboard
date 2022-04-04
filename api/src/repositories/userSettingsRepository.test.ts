f5ac523aa00e46b3b2ba08faa30ad91797396cc3
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-04 17:28:51 +0100

    Ripping out cache

--- api/src/repositories/userSettingsRepository.test.ts
+++ api/src/repositories/userSettingsRepository.test.ts
@@ -1,11 +1,9 @@
 import { UserSettingRepository } from './userSettingsRepository'
 import { IBaseMongoRepository } from './baseMongoRepository'
-import { IGenericCache } from '../caches/GenericCache'
 import { ok } from 'neverthrow'
 
 const expectedDbName = 'user'
 const expectedCollectionName = 'settings'
-const expectedCacheKey = `${expectedDbName}:${expectedCollectionName}`
 
 describe('UserSettingsRepository', () => {
   let fakeMongoRepo = {
@@ -13,14 +11,9 @@ describe('UserSettingsRepository', () => {
     replaceOneByFilter: jest.fn(),
     update: jest.fn()
   }
-  let fakeCache = {
-    GetByKey: jest.fn(),
-    SaveOnKey: jest.fn()
-  }
 
   let userSettingRepository = new UserSettingRepository(
-    fakeMongoRepo as unknown as IBaseMongoRepository,
-    fakeCache as unknown as IGenericCache
+    fakeMongoRepo as unknown as IBaseMongoRepository
   )
 
   beforeEach(() => {
@@ -29,17 +22,12 @@ describe('UserSettingsRepository', () => {
       replaceOneByFilter: jest.fn(),
       update: jest.fn()
     }
-    fakeCache = {
-      GetByKey: jest.fn(),
-      SaveOnKey: jest.fn()
-    }
     userSettingRepository = new UserSettingRepository(
-      fakeMongoRepo as unknown as IBaseMongoRepository,
-      fakeCache as unknown as IGenericCache
+      fakeMongoRepo as unknown as IBaseMongoRepository
     )
   })
   describe('getSettings', () => {
-    test('Has cache value :: returns cache value', async () => {
+    test('gets from repo', async () => {
       const userSetting = {
         userId: 'someUserId',
         settingId: 'SomeSettingId',
@@ -47,37 +35,16 @@ describe('UserSettingsRepository', () => {
           whoami: 'details'
         }
       }
-      fakeCache.GetByKey.mockResolvedValue({ value: userSetting })
-      const result = await userSettingRepository.getSetting(userSetting.userId, userSetting.settingId)
-      expect(result).toBe(userSetting)
-      expect(fakeCache.GetByKey).toBeCalledTimes(1)
-      expect(fakeCache.GetByKey).toBeCalledWith(`${expectedCacheKey}:${userSetting.userId}:${userSetting.settingId}`)
-      expect(fakeMongoRepo.getOneByFilter).toBeCalledTimes(0)
-      expect(fakeCache.SaveOnKey).toBeCalledTimes(0)
-    })
-    test('no cache value :: gets from repo, saves to cache', async () => {
-      const userSetting = {
-        userId: 'someUserId',
-        settingId: 'SomeSettingId',
-        details: {
-          whoami: 'details'
-        }
-      }
-      fakeCache.GetByKey.mockResolvedValue(undefined)
       fakeMongoRepo.getOneByFilter.mockResolvedValue(ok(userSetting))
       const result = await userSettingRepository.getSetting(userSetting.userId, userSetting.settingId)
       expect(result).toBe(userSetting)
-      expect(fakeCache.GetByKey).toBeCalledTimes(1)
-      expect(fakeCache.GetByKey).toBeCalledWith(`${expectedCacheKey}:${userSetting.userId}:${userSetting.settingId}`)
       expect(fakeMongoRepo.getOneByFilter).toBeCalledTimes(1)
       expect(fakeMongoRepo.getOneByFilter).toBeCalledWith(expectedDbName, expectedCollectionName, { userId: userSetting.userId, settingId: userSetting.settingId })
-      expect(fakeCache.SaveOnKey).toBeCalledTimes(1)
-      expect(fakeCache.SaveOnKey).toBeCalledWith(`${expectedCacheKey}:${userSetting.userId}:${userSetting.settingId}`, userSetting)
     })
   })
 
   describe('updateSetting', () => {
-    test('saves to mongo, then mongo value to cache', async () => {
+    test('saves to mongo', async () => {
       const userSetting = {
         userId: 'someUserId',
         settingId: 'SomeSettingId',
@@ -97,8 +64,6 @@ describe('UserSettingsRepository', () => {
       await userSettingRepository.updateSetting(userSetting.userId, userSetting.settingId, userSetting.details)
       expect(fakeMongoRepo.replaceOneByFilter).toBeCalledTimes(1)
       expect(fakeMongoRepo.replaceOneByFilter).toBeCalledWith(expectedDbName, expectedCollectionName, { userId: userSetting.userId, settingId: userSetting.settingId }, userSetting)
-      expect(fakeCache.SaveOnKey).toBeCalledTimes(1)
-      expect(fakeCache.SaveOnKey).toBeCalledWith(`${expectedCacheKey}:${userSetting.userId}:${userSetting.settingId}`, userSettingForCache)
     })
   })
 })
--- api/src/repositories/userSettingsRepository.ts
+++ api/src/repositories/userSettingsRepository.ts
@@ -1,4 +1,3 @@
-import * as baseGenericCache from '../caches/GenericCache'
 import * as baseMongoRepository from './baseMongoRepository'
 
 export interface IUserSetting {
@@ -10,28 +9,19 @@ export interface IUserSetting {
 export class UserSettingRepository {
   private readonly _cacheKey: string;
   private readonly _baseMongoRepo: baseMongoRepository.IBaseMongoRepository;
-  private readonly _baseGenericCache: baseGenericCache.IGenericCache
   private readonly _dbName: string = 'user'
   private readonly _collectionName: string = 'settings'
-  constructor (baseMongoRepo = baseMongoRepository, baseCache = baseGenericCache) {
+  constructor (baseMongoRepo = baseMongoRepository) {
     this._cacheKey = `${this._dbName}:${this._collectionName}`
     this._baseMongoRepo = baseMongoRepo
-    this._baseGenericCache = baseCache
   }
 
   public async getSetting (userId: string, settingId: string): Promise<IUserSetting> {
-    const cachedValue = await this._baseGenericCache.GetByKey<IUserSetting>(`${this._cacheKey}:${userId}:${settingId}`)
-    if (cachedValue) {
-      return cachedValue.value
-    }
     const result = await this._baseMongoRepo.getOneByFilter<IUserSetting>(this._dbName, this._collectionName, { userId, settingId })
-    // We should propogate neverthrow up through the stack
-    result.map(async userSetting => await this._baseGenericCache.SaveOnKey(`${this._cacheKey}:${userId}:${settingId}`, userSetting))
     return result.unwrapOr<IUserSetting>({ userId, settingId, details: null })
   }
 
   public async updateSetting (userId: string, settingId: string, details: any): Promise<void> {
-    const result = await this._baseMongoRepo.replaceOneByFilter(this._dbName, this._collectionName, { userId, settingId }, { userId, settingId, details })
-    result.map(async savedValue => await this._baseGenericCache.SaveOnKey(`${this._cacheKey}:${userId}:${settingId}`, savedValue))
+    await this._baseMongoRepo.replaceOneByFilter(this._dbName, this._collectionName, { userId, settingId }, { userId, settingId, details })
   }
 }
