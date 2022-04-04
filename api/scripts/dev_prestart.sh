ce20edcbd6eae2fe6a7f61f0e9953c215bd19cf5
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-04 22:03:09 +0100

    Replacing mongo with pg in spinup

--- api/scripts/dev_prestart.sh
+++ api/scripts/dev_prestart.sh
@@ -1,2 +1,4 @@
 docker start paceme-redis || docker run --name paceme-redis -d -p 6379:6379 redis:6.2.6
-docker start paceme-mongo || docker run --name paceme-mongo -d -p 27017:27017 mongo:4.4.13
+# Technically, we run cockroach, but it has the same protocol as PG 13
+# postgresql://paceme:paceme@localhost:5432/paceme
+docker start paceme-pg || docker run --name paceme-pg -e POSTGRES_USER=paceme -e POSTGRES_DB=paceme -e POSTGRES_PASSWORD=paceme -p 5432:5432 -d postgres:13
