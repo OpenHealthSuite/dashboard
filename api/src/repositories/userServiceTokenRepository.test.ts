7bb72ed803727e755bb263bb181604bac8b282af
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-04 17:26:53 +0100

    Ripping tokens out of cache

--- api/src/repositories/userServiceTokenRepository.test.ts
+++ api/src/repositories/userServiceTokenRepository.test.ts
@@ -1,12 +1,10 @@
 import { UserServiceTokenRepository } from './userServiceTokenRepository'
 import { IBaseMongoRepository } from './baseMongoRepository'
-import { IGenericCache } from '../caches/GenericCache'
 import { ok } from 'neverthrow'
 
 const expectedDbName = 'user'
 const expectedCollectionName = 'token'
 const expectedServiceId = 'someservicename'
-const expectedCacheKey = `${expectedDbName}:${expectedCollectionName}:${expectedServiceId}`
 
 describe('UserSettingsRepository', () => {
   let fakeMongoRepo = {
@@ -14,15 +12,10 @@ describe('UserSettingsRepository', () => {
     replaceOneByFilter: jest.fn(),
     update: jest.fn()
   }
-  let fakeCache = {
-    GetByKey: jest.fn(),
-    SaveOnKey: jest.fn()
-  }
 
   let userServiceTokenRepository = new UserServiceTokenRepository(
     expectedServiceId,
-    fakeMongoRepo as unknown as IBaseMongoRepository,
-    fakeCache as unknown as IGenericCache
+    fakeMongoRepo as unknown as IBaseMongoRepository
   )
 
   beforeEach(() => {
@@ -31,41 +24,20 @@ describe('UserSettingsRepository', () => {
       replaceOneByFilter: jest.fn(),
       update: jest.fn()
     }
-    fakeCache = {
-      GetByKey: jest.fn(),
-      SaveOnKey: jest.fn()
-    }
     userServiceTokenRepository = new UserServiceTokenRepository(
       expectedServiceId,
-      fakeMongoRepo as unknown as IBaseMongoRepository,
-      fakeCache as unknown as IGenericCache
+      fakeMongoRepo as unknown as IBaseMongoRepository
     )
   })
   describe('getUserToken', () => {
-    test('Has cache value :: returns cache value', async () => {
-      const userId = 'SomeUserjnsdf!"£123'
-      const userToken = { whoamI: 'userToken' }
-      fakeCache.GetByKey.mockResolvedValue({ value: userToken })
-      const result = await userServiceTokenRepository.getUserToken(userId)
-      expect(result).toBe(userToken)
-      expect(fakeCache.GetByKey).toBeCalledTimes(1)
-      expect(fakeCache.GetByKey).toBeCalledWith(`${expectedCacheKey}:${userId}`)
-      expect(fakeMongoRepo.getOneByFilter).toBeCalledTimes(0)
-      expect(fakeCache.SaveOnKey).toBeCalledTimes(0)
-    })
     test('no cache value :: gets from repo, saves to cache', async () => {
       const userId = 'SomeUserjnsdf!"£123'
       const userToken = { whoamI: 'userToken' }
-      fakeCache.GetByKey.mockResolvedValue(undefined)
       fakeMongoRepo.getOneByFilter.mockResolvedValue(ok({ token: userToken }))
       const result = await userServiceTokenRepository.getUserToken(userId)
       expect(result).toBe(userToken)
-      expect(fakeCache.GetByKey).toBeCalledTimes(1)
-      expect(fakeCache.GetByKey).toBeCalledWith(`${expectedCacheKey}:${userId}`)
       expect(fakeMongoRepo.getOneByFilter).toBeCalledTimes(1)
       expect(fakeMongoRepo.getOneByFilter).toBeCalledWith(expectedDbName, expectedCollectionName, { serviceId: expectedServiceId, userId })
-      expect(fakeCache.SaveOnKey).toBeCalledTimes(1)
-      expect(fakeCache.SaveOnKey).toBeCalledWith(`${expectedCacheKey}:${userId}`, userToken)
     })
   })
 
@@ -77,8 +49,6 @@ describe('UserSettingsRepository', () => {
       await userServiceTokenRepository.updateUserToken(userId, userToken)
       expect(fakeMongoRepo.replaceOneByFilter).toBeCalledTimes(1)
       expect(fakeMongoRepo.replaceOneByFilter).toBeCalledWith(expectedDbName, expectedCollectionName, { serviceId: expectedServiceId, userId }, { userId, serviceId: expectedServiceId, token: userToken })
-      expect(fakeCache.SaveOnKey).toBeCalledTimes(1)
-      expect(fakeCache.SaveOnKey).toBeCalledWith(`${expectedCacheKey}:${userId}`, userToken)
     })
   })
 })
--- api/src/repositories/userServiceTokenRepository.ts
+++ api/src/repositories/userServiceTokenRepository.ts
@@ -1,4 +1,3 @@
-import * as baseGenericCache from '../caches/GenericCache'
 import * as baseMongoRepository from './baseMongoRepository'
 
 export interface IUserServiceToken<T> {
@@ -11,28 +10,20 @@ export class UserServiceTokenRepository<T> {
   private readonly _serviceId: string;
   private readonly _cacheKey: string;
   private readonly _baseMongoRepo: baseMongoRepository.IBaseMongoRepository;
-  private readonly _baseGenericCache: baseGenericCache.IGenericCache
   private readonly _dbName: string = 'user'
   private readonly _collectionName: string = 'token'
-  constructor (serviceId: string, baseMongoRepo = baseMongoRepository, baseCache = baseGenericCache) {
+  constructor (serviceId: string, baseMongoRepo = baseMongoRepository) {
     this._serviceId = serviceId
     this._cacheKey = `${this._dbName}:${this._collectionName}:${this._serviceId}`
     this._baseMongoRepo = baseMongoRepo
-    this._baseGenericCache = baseCache
   }
 
   async getUserToken (userId: string): Promise<T | undefined> {
-    const cachedValue = await this._baseGenericCache.GetByKey<T>(`${this._cacheKey}:${userId}`)
-    if (cachedValue) {
-      return cachedValue.value
-    }
     const result = await this._baseMongoRepo.getOneByFilter<IUserServiceToken<T>>(this._dbName, this._collectionName, { serviceId: this._serviceId, userId })
-    result.map(async ({ token }) => await this._baseGenericCache.SaveOnKey(`${this._cacheKey}:${userId}`, token))
     return result.unwrapOr({ token: undefined }).token
   }
 
   async updateUserToken (userId: string, token: T) {
     await this._baseMongoRepo.replaceOneByFilter(this._dbName, this._collectionName, { userId, serviceId: this._serviceId }, { userId, serviceId: this._serviceId, token })
-    await this._baseGenericCache.SaveOnKey(`${this._cacheKey}:${userId}`, token)
   }
 }
