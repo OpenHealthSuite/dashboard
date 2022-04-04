d969fd25646978902cfd7111e939c8395260e6f1
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-04 21:01:37 +0100

    Update: Get using PG instead of mongo

--- api/src/repositories/userServiceTokenRepository.test.ts
+++ api/src/repositories/userServiceTokenRepository.test.ts
@@ -1,10 +1,7 @@
 import { UserServiceTokenRepository } from './userServiceTokenRepository'
 import { IBaseMongoRepository } from './baseMongoRepository'
-import { ok } from 'neverthrow'
 import { Pool } from 'pg'
 
-const expectedDbName = 'user'
-const expectedCollectionName = 'token'
 const expectedServiceId = 'someservicename'
 
 describe('UserSettingsRepository', () => {
@@ -39,26 +36,56 @@ describe('UserSettingsRepository', () => {
       fakePostgresPool as unknown as Pool
     )
   })
-  describe('getUserToken', () => {
-    test('no cache value :: gets from repo, saves to cache', async () => {
+
+  describe('createUserToken', () => {
+    test('saves to pg', async () => {
       const userId = 'SomeUserjnsdf!"£123'
       const userToken = { whoamI: 'userToken' }
-      fakeMongoRepo.getOneByFilter.mockResolvedValue(ok({ token: userToken }))
-      const result = await userServiceTokenRepository.getUserToken(userId)
-      expect(result).toBe(userToken)
-      expect(fakeMongoRepo.getOneByFilter).toBeCalledTimes(1)
-      expect(fakeMongoRepo.getOneByFilter).toBeCalledWith(expectedDbName, expectedCollectionName, { serviceId: expectedServiceId, userId })
+      fakePostgresPool.query.mockResolvedValue({ rowCount: 1, rows: [userToken] })
+      const expectedQuery = 'INSERT INTO user_service_token (service_id, user_id, token) VALUES ($1, $2, $3)'
+      const expectedArguments = [expectedServiceId, userId, userToken]
+      await userServiceTokenRepository.createUserToken(userId, userToken)
+      expect(fakePostgresPool.query).toBeCalledTimes(1)
+      expect(fakePostgresPool.query).toBeCalledWith(expectedQuery, expectedArguments)
     })
   })
 
-  describe('updateUserToken', () => {
-    test('saves to mongo, then mongo value to cache', async () => {
+  describe('getUserToken', () => {
+    test('gets from repo', async () => {
       const userId = 'SomeUserjnsdf!"£123'
       const userToken = { whoamI: 'userToken' }
-      fakeMongoRepo.replaceOneByFilter.mockResolvedValue(ok({ _id: '542c2b97bac0595474108b48', token: userToken }))
-      await userServiceTokenRepository.updateUserToken(userId, userToken)
-      expect(fakeMongoRepo.replaceOneByFilter).toBeCalledTimes(1)
-      expect(fakeMongoRepo.replaceOneByFilter).toBeCalledWith(expectedDbName, expectedCollectionName, { serviceId: expectedServiceId, userId }, { userId, serviceId: expectedServiceId, token: userToken })
+      const expectedQuery = 'SELECT token FROM user_service_token ust WHERE ust.service_id = $1 AND ust.user_id = $2'
+      const expectedArguments = [expectedServiceId, userId]
+      fakePostgresPool.query.mockResolvedValue({ rowCount: 1, rows: [{ token: userToken }] })
+      const result = await userServiceTokenRepository.getUserToken(userId)
+      expect(result.isOk()).toBeTruthy()
+      expect(result._unsafeUnwrap()).toBe(userToken)
+      expect(fakePostgresPool.query).toBeCalledTimes(1)
+      expect(fakePostgresPool.query).toBeCalledWith(expectedQuery, expectedArguments)
+    })
+    test('nothing found :: returns null', async () => {
+      const userId = 'SomeUserjnsdf!"£123'
+      const expectedQuery = 'SELECT token FROM user_service_token ust WHERE ust.service_id = $1 AND ust.user_id = $2'
+      const expectedArguments = [expectedServiceId, userId]
+      fakePostgresPool.query.mockResolvedValue({ rowCount: 0, rows: [] })
+      const result = await userServiceTokenRepository.getUserToken(userId)
+      expect(result.isOk()).toBeTruthy()
+      expect(result._unsafeUnwrap()).toBe(null)
+      expect(fakePostgresPool.query).toBeCalledTimes(1)
+      expect(fakePostgresPool.query).toBeCalledWith(expectedQuery, expectedArguments)
     })
   })
+
+  // describe('updateUserToken', () => {
+  //   test('saves to pg', async () => {
+  //     const userId = 'SomeUserjnsdf!"£123'
+  //     const userToken = { whoamI: 'userToken' }
+  //     fakePostgresPool.query.mockResolvedValue({ rowCount: 1, rows: [userSetting] })
+  //     const expectedQuery = 'UPDATE user_settings us SET us.details = $3 WHERE us.user_id = $1 AND us.setting_id = $2'
+  //     const expectedArguments = [userSetting.user_id, userSetting.setting_id, userSetting.details]
+  //     await userServiceTokenRepository.updateUserToken(userSetting.user_id, userSetting.setting_id, userSetting.details)
+  //     expect(fakePostgresPool.query).toBeCalledTimes(1)
+  //     expect(fakePostgresPool.query).toBeCalledWith(expectedQuery, expectedArguments)
+  //   })
+  // })
 })
--- api/src/repositories/userServiceTokenRepository.ts
+++ api/src/repositories/userServiceTokenRepository.ts
@@ -1,3 +1,4 @@
+import { err, ok, Result } from 'neverthrow'
 import { Pool } from 'pg'
 import * as baseMongoRepository from './baseMongoRepository'
 
@@ -10,21 +11,33 @@ export interface IUserServiceToken<T> {
 const userDbPool = new Pool()
 export class UserServiceTokenRepository<T> {
   private readonly _serviceId: string;
+  private readonly _tableName: string = 'user_service_token'
   private readonly _cacheKey: string;
   private readonly _baseMongoRepo: baseMongoRepository.IBaseMongoRepository;
-  private readonly _pgPool: Pool;
+  private readonly _postgresPool: Pool;
   private readonly _dbName: string = 'user'
   private readonly _collectionName: string = 'token'
   constructor (serviceId: string, baseMongoRepo = baseMongoRepository, pool: Pool = userDbPool) {
     this._serviceId = serviceId
     this._cacheKey = `${this._dbName}:${this._collectionName}:${this._serviceId}`
     this._baseMongoRepo = baseMongoRepo
-    this._pgPool = pool
+    this._postgresPool = pool
   }
 
-  async getUserToken (userId: string): Promise<T | undefined> {
-    const result = await this._baseMongoRepo.getOneByFilter<IUserServiceToken<T>>(this._dbName, this._collectionName, { serviceId: this._serviceId, userId })
-    return result.unwrapOr({ token: undefined }).token
+  async createUserToken (userId: string, token: T): Promise<Result<T, string>> {
+    try {
+      const insertQuery = `INSERT INTO ${this._tableName} (service_id, user_id, token) VALUES ($1, $2, $3)`
+      const result = await this._postgresPool.query(insertQuery, [this._serviceId, userId, token])
+      return result.rowCount > 0 ? ok(result.rows[0].token) : err('Nothing Inserted')
+    } catch (error: any) {
+      return err(error.message)
+    }
+  }
+
+  async getUserToken (userId: string): Promise<Result<T | null, string>> {
+    const selectQuery = `SELECT token FROM ${this._tableName} ust WHERE ust.service_id = $1 AND ust.user_id = $2`
+    const result = await this._postgresPool.query<{ token: T}>(selectQuery, [this._serviceId, userId])
+    return result.rowCount > 0 ? ok(result.rows[0].token) : ok(null)
   }
 
   async updateUserToken (userId: string, token: T) {
