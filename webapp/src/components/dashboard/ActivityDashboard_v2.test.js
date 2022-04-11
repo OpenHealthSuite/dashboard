849fbbebb580a547218234f64efc29f0569fbcce
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-11 21:21:07 +0100

    Test wrap setting retreival

--- webapp/src/components/dashboard/ActivityDashboard_v2.test.js
+++ webapp/src/components/dashboard/ActivityDashboard_v2.test.js
@@ -1,10 +1,99 @@
-import { render, screen } from '@testing-library/react';
-import { ActivityDashboard } from './ActivityDashboard_v2';
+import { render, screen } from "@testing-library/react";
+import { DEFAULT_DASHBOARD_SETTINGS } from "../../services/SettingsService";
+import {
+  ActivityDashboard,
+  getDashboardSettings,
+} from "./ActivityDashboard_v2";
 
-describe('ActivityDashboard', () => {
-  test('renders', () => {
+describe("ActivityDashboard", () => {
+  test("renders", () => {
     render(<ActivityDashboard />);
     const helloElement = screen.getByText(/Hello/i);
     expect(helloElement).toBeInTheDocument();
   });
-})
+});
+
+describe("ActivityDashboard functions", () => {
+  describe("getSettings", () => {
+    test("Happy path :: gets and returns settings", async () => {
+      const authDetails = { userId: "fakeUserId" };
+      const fakeAuthDetails = jest.fn().mockResolvedValue(authDetails);
+      const response = { whoami: "ReturnedSettings" };
+      const fakeFetch = jest.fn().mockResolvedValue({
+        status: 200,
+        json: jest.fn().mockResolvedValue(response),
+      });
+      const fakeApiRoot = "/fakeApiRoot";
+
+      const result = await getDashboardSettings({
+        fnGetAuthDetails: fakeAuthDetails,
+        fnFetch: fakeFetch,
+        apiRoot: fakeApiRoot,
+        defaults: {},
+      });
+
+      expect(result).toBe(response);
+      expect(fakeFetch).toBeCalledWith(
+        fakeApiRoot + "/users/" + authDetails.userId + "/userSettings/dashboard"
+      );
+    });
+    test("Non 200 status :: returns defaults", async () => {
+      const authDetails = { userId: "fakeUserId" };
+      const fakeAuthDetails = jest.fn().mockResolvedValue(authDetails);
+      const response = { whoami: "ReturnedSettings" };
+      const fakeFetch = jest.fn().mockResolvedValue({
+        status: 403,
+        json: jest.fn().mockResolvedValue(response),
+      });
+      const fakeApiRoot = "/fakeApiRoot";
+      const defaults = { whoami: "DefaultSettings" };
+
+      const result = await getDashboardSettings({
+        fnGetAuthDetails: fakeAuthDetails,
+        fnFetch: fakeFetch,
+        apiRoot: fakeApiRoot,
+        defaults: defaults,
+      });
+
+      expect(result).toBe(defaults);
+      expect(fakeFetch).toBeCalledTimes(1);
+      expect(fakeFetch).toBeCalledWith(
+        fakeApiRoot + "/users/" + authDetails.userId + "/userSettings/dashboard"
+      );
+    });
+    test("Error on auth :: returns defaults", async () => {
+      const authDetails = { userId: "fakeUserId" };
+      const fakeAuthDetails = jest.fn().mockRejectedValue(authDetails);
+      const fakeFetch = jest.fn();
+      const fakeApiRoot = "/fakeApiRoot";
+      const defaults = { whoami: "DefaultSettings" };
+
+      const result = await getDashboardSettings({
+        fnGetAuthDetails: fakeAuthDetails,
+        fnFetch: fakeFetch,
+        apiRoot: fakeApiRoot,
+        defaults: defaults,
+      });
+
+      expect(result).toBe(defaults);
+      expect(fakeFetch).toBeCalledTimes(0);
+    });
+    test("Error on user :: returns defaults", async () => {
+      const authDetails = { userId: "fakeUserId" };
+      const fakeAuthDetails = jest.fn().mockResolvedValue(authDetails);
+      const fakeFetch = jest.fn().mockRejectedValue({});
+      const fakeApiRoot = "/fakeApiRoot";
+      const defaults = { whoami: "DefaultSettings" };
+
+      const result = await getDashboardSettings({
+        fnGetAuthDetails: fakeAuthDetails,
+        fnFetch: fakeFetch,
+        apiRoot: fakeApiRoot,
+        defaults: defaults,
+      });
+
+      expect(result).toBe(defaults);
+      expect(fakeFetch).toBeCalledTimes(1);
+    });
+  });
+});
--- webapp/src/components/dashboard/ActivityDashboard_v2.tsx
+++ webapp/src/components/dashboard/ActivityDashboard_v2.tsx
@@ -1,10 +1,36 @@
-import './ActivityDashboard_v2.scss';
+import { getAuthDetails } from "../../services/AuthenticationDetails";
+import {
+  DEFAULT_DASHBOARD_SETTINGS,
+  IDashboardSettings,
+} from "../../services/SettingsService";
+import { API_ROOT } from "../../secrets";
+import "./ActivityDashboard_v2.scss";
+
+export async function getDashboardSettings({
+  fnGetAuthDetails = getAuthDetails,
+  fnFetch = fetch,
+  apiRoot = API_ROOT,
+  defaults = DEFAULT_DASHBOARD_SETTINGS,
+}) {
+  try {
+    const authDetails = await fnGetAuthDetails();
+    const response = await fnFetch(
+      apiRoot + "/users/" + authDetails.userId + "/userSettings/dashboard"
+    );
+    return response.status === 200
+      ? ((await response.json()) as IDashboardSettings)
+      : defaults;
+  } catch {
+    return defaults;
+  }
+}
 
 interface IDashboardProps {
-
+  //fnGetSettings: () => IDashboardSettings;
 }
 
 // eslint-disable-next-line no-empty-pattern
-export function ActivityDashboard({}: IDashboardProps) {
-  return (<div className="test">Hello</div>)
+export function ActivityDashboard({}: //fnGetSettings = getDashboardSettings,
+IDashboardProps) {
+  return <div className="test">Hello</div>;
 }
