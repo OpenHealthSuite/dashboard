0b914a2da62b9099cfe52ff156ce7d4b9dc92941
Lee <hello@leejohnmartin.co.uk>
2022-04-16 22:40:09 +0100

    New Base Tile

--- webapp/src/components/dashboard/tiles/DashboardTile.test.tsx
+++ webapp/src/components/dashboard/tiles/DashboardTile.test.tsx
@@ -26,7 +26,36 @@ describe("DashboardTile", () => {
     const cardContent = await screen.findByTestId("card-content")
     expect(cardContent).toContainElement(renderedCardContent)
   })
-  // Status Footer
-  // // Loading bar indicator
-  // // Current status
+  // Loading Icon
+  test("Loading Visible", async () => {
+    const loadingTestId = "card-loading"
+    render(<DashboardTile loading={true} ></DashboardTile>)
+    expect(await screen.findByTestId(loadingTestId)).toBeInTheDocument()
+  })
+  test("Loading Not Visible When False", async () => {
+    const loadingTestId = "card-loading"
+    render(<DashboardTile loading={false} ></DashboardTile>)
+    expect(screen.queryByTestId(loadingTestId)).not.toBeInTheDocument()
+  })
+  test("Loading Not Visible When Null", async () => {
+    const loadingTestId = "card-loading"
+    render(<DashboardTile></DashboardTile>)
+    expect(screen.queryByTestId(loadingTestId)).not.toBeInTheDocument()
+  })
+  // Error Icon
+  test("Error Visible", async () => {
+    const loadingTestId = "card-error"
+    render(<DashboardTile error={true} ></DashboardTile>)
+    expect(await screen.findByTestId(loadingTestId)).toBeInTheDocument()
+  })
+  test("Error Not Visible When False", async () => {
+    const loadingTestId = "card-error"
+    render(<DashboardTile error={false} ></DashboardTile>)
+    expect(screen.queryByTestId(loadingTestId)).not.toBeInTheDocument()
+  })
+  test("Error Not Visible When Null", async () => {
+    const loadingTestId = "card-error"
+    render(<DashboardTile></DashboardTile>)
+    expect(screen.queryByTestId(loadingTestId)).not.toBeInTheDocument()
+  })
 })
--- webapp/src/components/dashboard/tiles/DashboardTile.tsx
+++ webapp/src/components/dashboard/tiles/DashboardTile.tsx
@@ -3,14 +3,18 @@ import { CardHeader, Card, CardContent } from '@mui/material';
 interface IDashboardTileProps {
   headerText?: string
   children?: JSX.Element
+  loading?: boolean
+  error?: boolean
 }
 
-export function DashboardTile({ headerText, children }: IDashboardTileProps): JSX.Element {
+export function DashboardTile({ headerText, children, loading, error }: IDashboardTileProps): JSX.Element {
   const header = headerText ? <CardHeader data-testid="card-header" title={headerText} /> : null
   return <Card>
     {header}
     <CardContent data-testid="card-content">
       {children}
     </CardContent>
+    {loading && <div className="loading-indicator" data-testid="card-loading">Loading...</div>}
+    {error && <div className="error-indicator" data-testid="card-error">Error</div>}
   </Card>
 }
