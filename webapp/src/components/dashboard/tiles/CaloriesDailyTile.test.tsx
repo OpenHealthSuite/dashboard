1aeceddc853debf6a9d2bd8f8b52b3dfff201526
Lee <hello@leejohnmartin.co.uk>
2022-04-23 16:55:31 +0100

    Deleting even more code

--- webapp/src/components/dashboard/tiles/CaloriesDailyTile.test.tsx
+++ webapp/src/components/dashboard/tiles/CaloriesDailyTile.test.tsx
@@ -1,63 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import { getCaloriesForDay, CaloriesDailyTile } from "./CaloriesDailyTile";
+import { CaloriesDailyTile } from "./CaloriesDailyTile";
 
 describe("CaloriesDailyTile", () => {
   test("Renders itself as child of dashboard tile", () => {
     const dashboardtiletestid = "test-wrapper";
     const fakeDashboardTile = ({ children }: { children?: any }) => {
-        return <div data-testid={dashboardtiletestid}>{children}</div>
-      };
-    render(<CaloriesDailyTile FnDashboardTile={fakeDashboardTile} data={undefined}></CaloriesDailyTile>);
-    const wrapper = screen.getByTestId(dashboardtiletestid)
-    expect(wrapper).toBeInTheDocument()
-    expect(wrapper).toContainElement(screen.getByTestId("calories-daily-tile"))
+      return <div data-testid={dashboardtiletestid}>{children}</div>;
+    };
+    render(
+      <CaloriesDailyTile
+        FnDashboardTile={fakeDashboardTile}
+        data={undefined}
+      ></CaloriesDailyTile>
+    );
+    const wrapper = screen.getByTestId(dashboardtiletestid);
+    expect(wrapper).toBeInTheDocument();
+    expect(wrapper).toContainElement(screen.getByTestId("calories-daily-tile"));
   });
 });
-
-describe("CaloriesDailyTileFunctions", () => {
-  test("Happy path :: gets and returns settings", async () => {
-    const authDetails = { userId: "fakeUserId" };
-    const fakeAuthDetails = jest.fn().mockResolvedValue(authDetails);
-    const response = { whoami: "ReturnedSettings" };
-    const fakeFetch = jest.fn().mockResolvedValue({
-      status: 200,
-      json: jest.fn().mockResolvedValue(response),
-    });
-    const fakeApiRoot = "/fakeApiRoot";
-    const date = new Date();
-
-    const result = await getCaloriesForDay(
-      date,
-      fakeAuthDetails,
-      fakeFetch,
-      fakeApiRoot
-    );
-
-    expect(result).toBe(response);
-    expect(fakeFetch).toBeCalledWith(
-      fakeApiRoot + "/users/" + authDetails.userId + "/activities/" + date.toISOString().split('T')[0] + '/calories'
-    );
-  });
-  test("Non 200 status :: throws error", async () => {
-    const authDetails = { userId: "fakeUserId" };
-    const fakeAuthDetails = jest.fn().mockResolvedValue(authDetails);
-    const response = { whoami: "ReturnedSettings" };
-    const fakeFetch = jest.fn().mockResolvedValue({
-      status: 403,
-      json: jest.fn().mockResolvedValue(response),
-    });
-    const fakeApiRoot = "/fakeApiRoot";
-    const date = new Date();
-
-    await expect(getCaloriesForDay(
-      date,
-      fakeAuthDetails,
-      fakeFetch,
-      fakeApiRoot
-    )).rejects.not.toBeUndefined()
-    expect(fakeFetch).toBeCalledTimes(1);
-    expect(fakeFetch).toBeCalledWith(
-      fakeApiRoot + "/users/" + authDetails.userId + "/activities/" + date.toISOString().split('T')[0] + '/calories'
-    );
-  });
-})
--- webapp/src/components/dashboard/tiles/CaloriesDailyTile.tsx
+++ webapp/src/components/dashboard/tiles/CaloriesDailyTile.tsx
@@ -1,7 +1,6 @@
 import { DashboardTile, IDashboardTileProps } from "../DashboardTile";
-import { API_ROOT } from "../../../secrets";
-import { getAuthDetails } from "../../../services/AuthenticationDetails";
 import { useState } from "react";
+import { pacemeUserRouteGetRequest } from "../../../services/PaceMeApiService";
 
 const colors = {
   caloriesIn: "#3A3",
@@ -14,38 +13,24 @@ export interface ICalories {
   caloriesOut: number;
 }
 
-export async function getCaloriesForDay(
+async function getCaloriesForDay(
   day: Date,
-  fnGetAuthDetails = getAuthDetails,
-  fnFetch = fetch,
-  apiRoot = API_ROOT
+  fnFetch = pacemeUserRouteGetRequest
 ): Promise<ICalories> {
-  const authDetails = await fnGetAuthDetails();
-  const response = await fnFetch(
-    apiRoot +
-      "/users/" +
-      authDetails.userId +
-      "/activities/" +
-      day.toISOString().split("T")[0] +
-      "/calories"
+  return fnFetch(
+    "/activities/" + day.toISOString().split("T")[0] + "/calories"
   );
-  if (response.status === 200) {
-    return (await response.json()) as ICalories;
-  } else {
-    throw new Error("Error retrieving calories");
-  }
 }
 
 interface ICaloriesDailyTileProps {
-  data: undefined | ICalories
+  data: undefined | ICalories;
   FnDashboardTile?: (props: IDashboardTileProps<ICalories>) => JSX.Element;
-  FnGetCalories?:  (
+  FnGetCalories?: (
     setIsErrored: (err: boolean) => void,
     setIsLoading: (lod: boolean) => void,
     setCalories: (data: ICalories | undefined) => void
   ) => Promise<void>;
 }
-
 
 const getCaloriesDataGetter = (
   setIsErrored: (err: boolean) => void,
