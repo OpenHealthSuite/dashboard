3bc6baa00d0c02076438f9e3f936cd1e38775f3e
Lee <hello@leejohnmartin.co.uk>
2022-04-24 08:40:09 +0100

    Adding Post requests to the new API Service

--- webapp/src/services/PaceMeApiService.test.ts
+++ webapp/src/services/PaceMeApiService.test.ts
@@ -1,22 +1,46 @@
-import { pacemeUserRouteGetRequest, pacemeGetRequest } from "./PaceMeApiService";
+import { pacemeUserRouteGetRequest, pacemeGetRequest, pacemePostRequest, pacemeUserRoutePostRequest } from "./PaceMeApiService";
 
 describe("pacemeUserRouteGetRequest", () => {
   test("Gets user details and prepends to request path", async () => {
     const authDetails = { userId: "fakeUserId" };
     const fakeAuthDetails = jest.fn().mockResolvedValue(authDetails);
     const response = { whoami: "ReturnedSettings" };
-    const fakeFetch = jest.fn().mockResolvedValue(response);
+    const fakePacemeGetRequest = jest.fn().mockResolvedValue(response);
     const fakeRequestRoute = "/fakeRequestRoute";
 
     const result = await pacemeUserRouteGetRequest(
       fakeRequestRoute,
       fakeAuthDetails,
-      fakeFetch
+      fakePacemeGetRequest
     );
 
     expect(result).toBe(response);
-    expect(fakeFetch).toBeCalledWith(
+    expect(fakePacemeGetRequest).toBeCalledWith(
        "/users/" + authDetails.userId + fakeRequestRoute
+    );
+  });
+})
+
+describe("pacemeUserRoutePostRequest", () => {
+  test("Gets user details and prepends to request path", async () => {
+    const authDetails = { userId: "fakeUserId" };
+    const fakeAuthDetails = jest.fn().mockResolvedValue(authDetails);
+    const response = { whoami: "ReturnedSettings" };
+    const fakePacemePostRequest = jest.fn().mockResolvedValue(response);
+    const fakeRequestRoute = "/fakeRequestRoute";
+    const fakeRequestBody = { whoami: "FakeRequestBody"}
+
+    const result = await pacemeUserRoutePostRequest(
+      fakeRequestRoute,
+      fakeRequestBody,
+      fakeAuthDetails,
+      fakePacemePostRequest
+    );
+
+    expect(result).toBe(response);
+    expect(fakePacemePostRequest).toBeCalledWith(
+       "/users/" + authDetails.userId + fakeRequestRoute,
+       fakeRequestBody
     );
   });
 })
@@ -59,6 +83,60 @@ describe("pacemeGetRequest", () => {
     expect(fakeFetch).toBeCalledTimes(1);
     expect(fakeFetch).toBeCalledWith(
       fakeApiRoot + fakeRequestRoute
+    );
+  });
+})
+
+describe("pacemePostRequest", () => {
+  test("Happy path :: uses URL, uses body, rereives data", async () => {
+    const response = { whoami: "ReturnedSettings" };
+    const fakeRequestBody = { whoami: "FakeRequestBody"}
+    const fakeFetch = jest.fn().mockResolvedValue({
+      status: 200,
+      json: jest.fn().mockResolvedValue(response),
+    });
+    const fakeRequestRoute = "/fakeRequestRoute";
+    const fakeApiRoot = "http://localhost:9090";
+
+    const result = await pacemePostRequest(
+      fakeRequestRoute,
+      fakeRequestBody,
+      fakeFetch,
+      fakeApiRoot
+    );
+
+    expect(result).toBe(response);
+    expect(fakeFetch).toBeCalledWith(
+      fakeApiRoot + fakeRequestRoute,
+      {
+        method: "POST",
+        body: JSON.stringify(fakeRequestBody)
+      }
+    );
+  });
+  test("Non 200 status :: throws error", async () => {
+    const response = { whoami: "ReturnedSettings" };
+    const fakeFetch = jest.fn().mockResolvedValue({
+      status: 403,
+      json: jest.fn().mockResolvedValue(response),
+    });
+    const fakeRequestRoute = "/fakeRequestRoute";
+    const fakeApiRoot = "http://localhost:9090";
+    const fakeRequestBody = { whoami: "FakeRequestBody"}
+
+    await expect(pacemePostRequest(
+      fakeRequestRoute,
+      fakeRequestBody,
+      fakeFetch,
+      fakeApiRoot
+    )).rejects.not.toBeUndefined()
+    expect(fakeFetch).toBeCalledTimes(1);
+    expect(fakeFetch).toBeCalledWith(
+      fakeApiRoot + fakeRequestRoute,
+      {
+        method: "POST",
+        body: JSON.stringify(fakeRequestBody)
+      }
     );
   });
 })
--- webapp/src/services/PaceMeApiService.ts
+++ webapp/src/services/PaceMeApiService.ts
@@ -10,12 +10,41 @@ export async function pacemeUserRouteGetRequest<T>(
   return fnPacemeGetRequest<T>('/users/' + response.userId + path)
 }
 
+export async function pacemeUserRoutePostRequest<R, T>(
+  path: string,
+  body: R,
+  fnGetAuthDetails = getAuthDetails,
+  fnPacemePostRequest = pacemePostRequest
+): Promise<T> {
+  const response = await fnGetAuthDetails();
+  return fnPacemePostRequest<R, T>('/users/' + response.userId + path, body)
+}
+
 export async function pacemeGetRequest<T>(
   path: string,
   fnFetch = fetch,
   apiRoot = API_ROOT
 ): Promise<T> {
   const response = await fnFetch(apiRoot + path);
+  if (response.status === 200) {
+    return await response.json() as T;
+  } else {
+    throw new Error(`HTTP Status ${response.status}: Error retrieving data`);
+  }
+}
+
+
+
+export async function pacemePostRequest<R, T>(
+  path: string,
+  body: R,
+  fnFetch = fetch,
+  apiRoot = API_ROOT
+): Promise<T> {
+  const response = await fnFetch(apiRoot + path, {
+    method: "POST",
+    body: JSON.stringify(body)
+  });
   if (response.status === 200) {
     return await response.json() as T;
   } else {
