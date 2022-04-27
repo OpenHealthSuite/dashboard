dbb9cff809f945fdf51de9e7c9a876404f228731
Lee <hello@leejohnmartin.co.uk>
2022-04-27 07:58:40 +0100

    Changing off default postgres port for dev env

--- api/scripts/dev_prestart.sh
+++ api/scripts/dev_prestart.sh
@@ -1,4 +1,4 @@
 docker start paceme-redis || docker run --name paceme-redis -d -p 6379:6379 redis:6.2.6
-# Technically, we run cockroach, but it has the same protocol as PG 13
 # postgresql://paceme:paceme@localhost:5432/paceme
-docker start paceme-pg || docker run --name paceme-pg -e POSTGRES_USER=paceme -e POSTGRES_DB=paceme -e POSTGRES_PASSWORD=paceme -p 5432:5432 -d postgres:13
+# I always forget this, but the ports are HOST:CONTAINER
+docker start paceme-pg || docker run --name paceme-pg -e POSTGRES_USER=paceme -e POSTGRES_DB=paceme -e POSTGRES_PASSWORD=paceme -p 2345:5432 -d postgres:13
--- api/src/repositories/configuredDbPool.ts
+++ api/src/repositories/configuredDbPool.ts
@@ -1,5 +1,5 @@
 import { Pool } from 'pg'
 
 export const configuredDbPool = new Pool({
-  connectionString: process.env.PG_CONNECTION_STRING || 'postgresql://paceme:paceme@localhost:5432/paceme'
+  connectionString: process.env.PG_CONNECTION_STRING || 'postgresql://paceme:paceme@localhost:2345/paceme'
 })
