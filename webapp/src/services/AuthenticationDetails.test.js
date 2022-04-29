32f1d14eb5bf46fc890b67aef6f39f82403c3910
Lee <hello@leejohnmartin.co.uk>
2022-04-29 14:38:13 +0100

    Whoops: Forgot a test

--- webapp/src/services/AuthenticationDetails.test.js
+++ webapp/src/services/AuthenticationDetails.test.js
@@ -11,20 +11,12 @@ describe("AuthenticationDetails function", () => {
   afterAll(() => {
     process.env = OLD_ENV;
   });
-  test.each(['val2', 'something', 'hello'])('Dev environment variable defined :: returns defined value', async (devVal) => {
-    const fakeFetch = jest.fn()
-    process.env.REACT_APP_DEV_USER_ID = devVal
-    const result = await getAuthDetails(fakeFetch, devVal)
-    expect(fakeFetch).not.toBeCalled()
-    expect(result.userId).toBe(devVal)
-  })
-  test.each(["something", "userelse", "userID"])('Non dev environment :: fetches from remote', async (userValue) => {
-    const fakeJson = jest.fn().mockResolvedValue({ user: userValue })
-    const fakeFetch = jest.fn().mockResolvedValue({ json: fakeJson })
+  test.each(["something", "userelse", "userID"])('Calls /whoami for user details', async (userValue) => {
+    const fakePacemeGet = jest.fn().mockResolvedValue({ userId: userValue })
     process.env.REACT_APP_DEV_USER_ID = undefined
-    const result = await getAuthDetails(fakeFetch)
-    expect(fakeFetch).toBeCalledTimes(1)
-    expect(fakeFetch).toBeCalledWith('/oauth2/userinfo')
+    const result = await getAuthDetails(fakePacemeGet)
+    expect(fakePacemeGet).toBeCalledTimes(1)
+    expect(fakePacemeGet).toBeCalledWith('/whoami')
     expect(result.userId).toBe(userValue)
   })
 })
