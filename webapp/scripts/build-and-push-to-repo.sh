cdb90f8ba478721bfa7d4eabea6a0558d44ecf0a
Lee Martin <hello@leejohnmartin.co.uk>
2022-09-10 21:55:36 +0100

    Removed: Old dockerfiles

--- webapp/scripts/build-and-push-to-repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Expecting to be run from the root
-export REPOSITORY="leemartin77/paceme-webapp"
-export VERSION="$(cat package.json | jq -r '.version')"
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  -t "$REPOSITORY:latest" -t "$REPOSITORY:$VERSION" --push . 
