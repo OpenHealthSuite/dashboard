28538faffaf4a8a752b8fa2999914f58a95c9cac
Lee <hello@leejohnmartin.co.uk>
2022-09-10 21:10:08 +0100

    Update: Crude switch to combined image

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,4 @@ helm upgrade paceme ./helm \
   --set "fitbit.clientSecret=$(credstash get paceme/fitbit-client-secret)" \
   --set "api.postgrespw=$(credstash get paceme/postgrespw)" \
   --set "api.postgresconnectionstring=postgresql://paceme:$(credstash get paceme/postgrespw)@postgres.paceme.svc:5432/paceme" \
-  --set "api.tag=$(cat api/package.json | jq -r '.version')" \
-  --set "webapp.tag=$(cat webapp/package.json | jq -r '.version')" 
+  --set "api.tag=v0.0.3"
