9e93cfaea4671376e84aab0378e941d645fd2ce4
Lee <hello@leejohnmartin.co.uk>
2022-09-26 21:25:34 +0100

    Update: Remove deploy script tag

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,4 @@ helm upgrade paceme ./helm \
   --set "fitbit.clientId=$(credstash get paceme/fitbit-client-id)" \
   --set "fitbit.clientSecret=$(credstash get paceme/fitbit-client-secret)" \
   --set "api.postgrespw=$(credstash get paceme/postgrespw)" \
-  --set "api.postgresconnectionstring=postgresql://paceme:$(credstash get paceme/postgrespw)@postgres.paceme.svc:5432/paceme" \
-  --set "api.tag=v0.0.5"
+  --set "api.postgresconnectionstring=postgresql://paceme:$(credstash get paceme/postgrespw)@postgres.paceme.svc:5432/paceme"
--- deploy_test.sh
+++ deploy_test.sh
@@ -10,5 +10,4 @@ helm upgrade paceme-test ./helm \
   --set "fitbit.clientSecret=$(credstash get paceme/fitbit-client-secret)" \
   --set "api.postgrespw=$(credstash get paceme/postgrespw)" \
   --set "api.postgresconnectionstring=postgresql://paceme:$(credstash get paceme/postgrespw)@postgres.paceme-test.svc:5432/paceme" \
-  --set "api.tag=v0.0.5" \
   --set "hostname=app-test.paceme.info"
