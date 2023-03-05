a1d55301a8f406a01862ec20ddce5180e0dc1ad2
Lee Martin <hello@leejohnmartin.co.uk>
2023-03-05 17:14:54 +0000

    Fix: Must return JSON

--- api/src/handlers/userSettingHandlers.ts
+++ api/src/handlers/userSettingHandlers.ts
@@ -16,13 +16,13 @@ export function addUserSettingHandlers (app: Application) {
 const settingsGet = async (userId: string, req: Request, res: Response) => {
   const settingId = req.params.settingId
   if (!Object.values(SettingKeys).includes(settingId)) {
-    return res.status(400).send(`Setting "${settingId}" Not Found`)
+    return res.status(400).send({ error: `Setting "${settingId}" Not Found` })
   }
 
   const planRes = await repository.getSetting(userId, settingId)
 
-  planRes.map(plan => plan ? res.send(plan.details) : res.status(404).send(`No item for user ${userId} found under ${settingId}`))
-    .mapErr(() => res.status(500).send('Internal Error'))
+  planRes.map(plan => plan ? res.send(plan.details) : res.status(404).send({ error: `No item for user ${userId} found under ${settingId}` }))
+    .mapErr(() => res.status(500).send({ error: 'Internal Error' }))
 }
 
 const settingsUpdate = async (userId: string, req: Request, res: Response) => {
@@ -30,15 +30,15 @@ const settingsUpdate = async (userId: string, req: Request, res: Response) => {
   const settingId = req.params.settingId
 
   if (!Object.values(SettingKeys).includes(settingId)) {
-    return res.status(400).send(`Setting "${settingId}" Not Found`)
+    return res.status(400).send({ error: `Setting "${settingId}" Not Found` })
   }
 
   (await repository.getSetting(userId, settingId))
     .map(async existing => {
       if (existing) {
-        return (await repository.updateSetting(userId, settingId, body)).map(() => res.send('OK - Updated'))
+        return (await repository.updateSetting(userId, settingId, body)).map(() => res.send({ msg: 'OK - Updated' }))
       }
-      return (await repository.createSetting(userId, settingId, body)).map(() => res.send('OK - Created'))
+      return (await repository.createSetting(userId, settingId, body)).map(() => res.send({ msg: 'OK - Created' }))
     })
-    .mapErr(() => res.status(500).send('Internal Error'))
+    .mapErr(() => res.status(500).send({ error: 'Internal Error' }))
 }
