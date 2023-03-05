5dc89aabb787c0ac556d16565c6a9c2c482a392d
Lee Martin <hello@leejohnmartin.co.uk>
2023-03-05 17:02:30 +0000

    Removed: Unneccessary + erroring helmet

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet";
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
@@ -8,14 +7,6 @@ import { Root } from './components/Root'
 function App() {
   return (
     <Router>
-      <Helmet>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Helmet>
       <Root />
     </Router>
     
