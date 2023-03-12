b8e4a7d112d0e45da2cfb1598a42e287b0cc8834
Lee Martin <hello@leejohnmartin.co.uk>
2023-03-12 17:35:06 +0000

    Update: Pull out final secret from deploy script
    [skip-ci]

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
-PGPWD=$(kubectl get secret/postgres-service -n service -o jsonpath="{.data.postgrespw}" | base64 --decode)
-
 helm upgrade paceme ./helm \
   --namespace paceme \
   --create-namespace \
   --install \
-  --atomic \
-  --set "fitbit.clientId=$(credstash get paceme/fitbit-client-id)" \
-  --set "fitbit.clientSecret=$(credstash get paceme/fitbit-client-secret)"
+  --atomic
