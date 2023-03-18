63eb6d9be735c8a1636be6817c9541f13f8f1d49
Lee Martin <hello@leejohnmartin.co.uk>
2023-03-18 20:28:06 +0000

    Styling: Dashboard reactivity

--- webapp/src/components/dashboard/ActivityDashboard.tsx
+++ webapp/src/components/dashboard/ActivityDashboard.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AvailableTiles, IAvailableTiles } from "./tiles";
 import { DashboardSettingsContext } from "../../App";
 import { Flex } from "@chakra-ui/react";
@@ -13,31 +13,47 @@ export function ActivityDashboard({
 }: IDashboardProps) {
   const settingsContext = useContext(DashboardSettingsContext);
   const dashboardSettings = settingsContext?.dashboardSettings;
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   if (settingsContext && dashboardSettings) {
     return (
-      <Flex display="grid"
-        gridTemplateColumns="repeat(3, minmax(200px, 1fr))"
-        gridAutoRows="minmax(100px, auto)"
-        gap="1em"
-        padding="1em"
-        >
-        {dashboardSettings.tileSettings
-          .filter((ts) =>
-            Object.keys(availableTiles).includes(ts.componentName)
-          )
-          .map((ts) => availableTiles[ts.componentName])
-          .map((tile, i) => {
-            return (
-              <Flex
-                border="1px solid gray"
-                borderRadius="1em"
-                padding="1em"
-                key={`gridkey-${i}`}
-              >
-                <tile.component />
-              </Flex>
-            );
-          })}
+      <Flex width={"100%"} flexDirection={'column'} alignItems={'center'}>
+        <Flex display="grid"
+          maxWidth={"1280px"}
+          gridTemplateColumns={`repeat(${Math.min(Math.floor(screenWidth / 320), 3)}, minmax(320px, 1fr))`}
+          gridAutoRows="minmax(100px, auto)"
+          gap="1em"
+          padding="1em"
+          >
+          {dashboardSettings.tileSettings
+            .filter((ts) =>
+              Object.keys(availableTiles).includes(ts.componentName)
+            )
+            .map((ts) => availableTiles[ts.componentName])
+            .map((tile, i) => {
+              return (
+                <Flex
+                  border="1px solid gray"
+                  borderRadius="1em"
+                  padding="1em"
+                  key={`gridkey-${i}`}
+                >
+                  <tile.component />
+                </Flex>
+              );
+            })}
+        </Flex>
       </Flex>
     );
   }
