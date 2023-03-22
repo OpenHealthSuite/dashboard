0b81088b015f6fa13cc5e0a0b547f0b1eca284c8
Lee Martin <hello@leejohnmartin.co.uk>
2023-03-22 19:46:32 +0000

    Tweak: Add logic for fitbit being configured

--- api/src/providers/index.ts
+++ api/src/providers/index.ts
@@ -1,9 +1,12 @@
-import { fitbitDataProvider as fitbit } from './Fitbit/FitbitStatProvider'
+import { fitbitDataProvider as fitbitProvider } from './Fitbit/FitbitStatProvider'
 import { openFoodDiaryDataProvider as ofdProvider } from './OpenFoodDiary/OpenFoodDiaryProvider'
 
 const ofdConfigured = process.env.OPEN_FOOD_DIARY_API && process.env.OPEN_FOOD_DIARY_API_HEADER
 
+const fitbitConfigured = process.env.FITBIT_CLIENT_ID && process.env.FITBIT_CLIENT_SECRET
+
 const openfooddiary = ofdConfigured ? ofdProvider : {}
+const fitbit = fitbitConfigured ? fitbitProvider : {}
 export {
   fitbit,
   openfooddiary
